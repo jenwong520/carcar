7c704a0bd31e1deb003827e60e5df00e42356fa3
Jennifer Wong <jmw520@gmail.com>
2024-08-13 15:35:37 -0700

    Delete technician function completed

--- service/api/service_rest/api_urls.py
+++ service/api/service_rest/api_urls.py
@@ -1,6 +1,7 @@
-from .views import api_list_technicians
+from .views import api_list_technicians, api_technician
 from django.urls import path
 
 urlpatterns = [
     path("technicians/", api_list_technicians, name="api_list_technicians"),
+    path("technicians/<int:pk>/", api_technician, name="api_show_technician"),
 ]
--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -31,3 +31,48 @@ def api_list_technicians(request):
             )
             response.status_code = 400
             return response
+
+@require_http_methods(["DELETE", "GET", "PUT"])
+def api_technician(request, pk):
+    if request.method == "GET":
+        try:
+            technician = Technician.objects.get(id=pk)
+            return JsonResponse(
+                technician,
+                encoder=TechnicianEncoder,
+                safe=False
+            )
+        except Technician.DoesNotExist:
+            response = JsonResponse({"message": "Does not exist"})
+            response.status_code = 404
+            return response
+    elif request.method == "DELETE":
+        try:
+            technician = Technician.objects.get(id=pk)
+            technician.delete()
+            return JsonResponse(
+                technician,
+                encoder=TechnicianEncoder,
+                safe=False,
+            )
+        except Technician.DoesNotExist:
+            return JsonResponse({"message": "Does not exist"})
+    else: # PUT
+        try:
+            content = json.loads(request.body)
+            technician = Technician.objects.get(id=pk)
+
+            props = ["first_name", "last_name", "employee_id"]
+            for prop in props:
+                if prop in content:
+                    setattr(technician, prop, content[prop])
+            technician.save()
+            return JsonResponse(
+                technician,
+                encoder=TechnicianEncoder,
+                safe=False,
+            )
+        except Technician.DoesNotExist:
+            response = JsonResponse({"message": "Does not exist"})
+            response.status_code = 404
+            return response
