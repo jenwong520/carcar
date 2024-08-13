5f334e392c4c05142c18efc767de9e6010dd11fb
Rachel <rachelmariahmcreynolds@gmail.com>
2024-08-12 20:14:51 -0600

    Began README, Fixed Windows Thing For Polling, & Finished Vehicle Models List

--- ghi/app/src/App.jsx
+++ ghi/app/src/App.jsx
@@ -1,14 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./MainPage";
 import Nav from "./Nav";
+import VehicleModelsList from './VehicleModelsList';
+// import VehicleModelsForm from './VehicleModelsForm';
 
-function App() {
+function App(props) {
+	if (props.models === undefined) {
+		return null;
+	}
 	return (
 		<BrowserRouter>
 			<Nav />
 			<div className="container">
 				<Routes>
 					<Route path="/" element={<MainPage />} />
+					<Route path="/models" element={<VehicleModelsList models={props.models} />} />
+					{/* <Route path="/models/create" element={<VehicleModelsForm />} /> */}
 				</Routes>
 			</div>
 		</BrowserRouter>
--- ghi/app/src/main.jsx
+++ ghi/app/src/main.jsx
@@ -4,8 +4,18 @@ import App from "./App.jsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+async function loadModels() {
+	const response = await fetch('http://localhost:8100/api/models/');
+	if (response.ok) {
+		const data = await response.json();
+		// console.log(data);
+		ReactDOM.createRoot(document.getElementById('root')).render(
+			<React.StrictMode>
+				<App models={data.models} />
+			</React.StrictMode>
+		);
+	  } else {
+		console.error(response);
+	  }
+	}
+  loadModels();
